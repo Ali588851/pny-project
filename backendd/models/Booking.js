923af3cc1638074c8e6a8907c083477047ad63a0
Ali Husnain <ah4642767@gmail.com>
2025-05-27 19:04:36 +0500

    Update Booking.js

--- backendd/models/Booking.js
+++ backendd/models/Booking.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const BookingSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  department: { type: String, required: true },
-  day: { type: String, required: true },
-  time: { type: String, required: true },
+  name: String,
+  email: String,
+  phone: String,
+  department: String,
+  date: Date,    // just the type here
+  time: String
 }, { timestamps: true });
 
 module.exports = mongoose.model('Booking', BookingSchema);
+
